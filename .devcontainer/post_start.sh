c5f6092e6b5586203adad2b77328b8d5020c176b
lanzhao88 <42985407+lanzhao88@users.noreply.github.com>
2026-07-22 22:54:36 -0400

    Comment out GPU setup in post_start.sh
    Disable GPU full-system setup steps to save storage in Codespaces.

--- .devcontainer/post_start.sh
+++ .devcontainer/post_start.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
-if [ ! -f /tmp/x86-ubuntu-gpu-ml-isca ]; then
-  cp x86-ubuntu-gpu-ml-isca.gz /tmp
-  gunzip /tmp/x86-ubuntu-gpu-ml-isca.gz
-fi
+# [Lan] GPU FS-mode disabled to save storage in Codespaces
+# (This script normally unpacks a 55GB GPU disk image into /tmp)
+# All GPU full-system setup steps are skipped.
 
-ln -s /tmp/x86-ubuntu-gpu-ml-isca .
+# if [ ! -f /tmp/x86-ubuntu-gpu-ml-isca ]; then
+#  cp x86-ubuntu-gpu-ml-isca.gz /tmp
+#  gunzip /tmp/x86-ubuntu-gpu-ml-isca.gz
+# fi
+
+# ln -s /tmp/x86-ubuntu-gpu-ml-isca .
